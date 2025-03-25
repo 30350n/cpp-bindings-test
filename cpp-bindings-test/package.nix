99dde1371fed4edaca5b07e0e560b3034e866ba5
Bobbe <maxschlecht@web.de>
2025-03-26 00:21:59 +0100

    Fix package src regex

--- cpp-bindings-test/package.nix
+++ cpp-bindings-test/package.nix
@@ -10,7 +10,7 @@ stdenv.mkDerivation {
         "^include.*"
         "^src.*"
         "^test.*"
-        "CMakeLists.txt"
+        "CMakeLists\.txt"
         ".*\.pc\.in"
     ];
     nativeBuildInputs = [cmake];

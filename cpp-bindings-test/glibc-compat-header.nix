cab01e9ce85c92e43e4847dfc0b772aec1349f02
Bobbe <maxschlecht@web.de>
2025-05-26 13:42:11 +0200

    Try glibc 2.26

--- cpp-bindings-test/glibc-compat-header.nix
+++ cpp-bindings-test/glibc-compat-header.nix
@@ -5,10 +5,10 @@
     binutils,
     python3,
 }: let
-    glibc-2-24 =
+    glibc-2-26 =
         (import (builtins.fetchTarball {
-            url = "https://github.com/NixOS/nixpkgs/archive/c0c50dfcb70d48e5b79c4ae9f1aa9d339af860b4.tar.gz";
-            sha256 = "17p3w4mgfr4yj2p0jz6kqgzhyr04h4fap5hnd837664xd1xhwdjb";
+            url = "https://github.com/NixOS/nixpkgs/archive/0b307aa73804bbd7a7172899e59ae0b8c347a62d.tar.gz";
+            sha256 = "1fr9fn0iv3qxdqcrnfg7l0y22s91rc9icfm0027q02245vx558p6";
         }) {inherit system;})
         .glibc;
     glibc-compat = fetchFromGitHub {
@@ -22,7 +22,7 @@ in
         nativeBuildInputs = [binutils python3];
     } ''
         mkdir -p $out/include
-        objdump -T ${glibc-2-24}/lib/libc.so.6 \
+        objdump -T ${glibc-2-26}/lib/libc.so.6 \
             | python3 ${glibc-compat}/gensym.py \
             > $out/include/glibc-compat.h
     ''

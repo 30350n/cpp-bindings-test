9dd4a4f2faac2956d838285ff0be61b8a6a18384
Bobbe <maxschlecht@web.de>
2025-06-10 19:53:46 +0200

    Use std::vector in test code

--- cpp-bindings-test/src/main.cpp
+++ cpp-bindings-test/src/main.cpp
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include <vector>
 
 extern "C" int hello(int num)
 {
     printf("Hello %d\n", num);
     fflush(stdout);
     return 0;
+}
+
+std::vector<int> a;
+
+extern "C" std::vector<int> *foo()
+{
+    a.resize(20);
+    for (int i = 0; i < 10; i++)
+    {
+        a.push_back(i);
+    }
+    return &a;
 }
